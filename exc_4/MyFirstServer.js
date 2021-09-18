96f45be39916b480e5e71ab93ddcca87c9d9240f
Shir Menashe <shirmenashe@h-MacBook-Air-sl-syr.local>
2021-09-19 00:11:41 +0300

    commit changes of exc 6 on exc 4 files

--- exc_4/MyFirstServer.js
+++ exc_4/MyFirstServer.js
@@ -1,6 +1,34 @@
-var http = require('http');
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const sql = require("/Users/shirmenash/shir.menashe/exc_6/db.js")
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
-}).listen(8080);
+// parse requests of contenttype: application/json
+app.use(bodyParser.json());
+// parse requests of contenttype: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true
+}));
+
+// simple route
+app.get("/", (req, res) => {
+res.json({ message: "Welcome to web course example application." });
+});
+
+// Create a route for getting all customers
+app.get("/customers", function(req, res){
+    sql.query("SELECT * FROM customers", (err, mysqlres) => {
+        if (err) {
+        console.log("error: ", err);
+        res.status(400).send({message: "error in getting all customers: " + err});
+        return;
+        }
+        console.log("got all customers!");
+        res.send(mysqlres);
+        return;
+    });
+});
+
+// set port, listen for requests
+app.listen(3000, () => {
+    console.log("Server is running on port 3000.");
+});
